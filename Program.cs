dd92a5cc68530469b6135a47ead763a91d8f7c2c
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-17 15:43:01 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("as vezes no silencio da noite!");
+            Console.WriteLine("eu fico imaginando nós dois!");
+            Console.WriteLine("eu fico ali sonhando acordado!");
+            Console.WriteLine("juntando o antes o agora o depois!");
+            Console.WriteLine("");
+            Console.WriteLine("porque você me deixa tão solto!");
+            Console.WriteLine("porque você não coloca em mim!");
+            Console.WriteLine("to me sentindo muito sozinho!");
+            Console.WriteLine("");
+            Console.WriteLine("não sou nem quero ser o seu dono!");
+            Console.WriteLine("é que um carinho as vezes cai bem!");
+            Console.WriteLine("eu tenho os meus desejos e planos secretos!");
+            Console.WriteLine("só abro pra você mais ninguém!");
+            Console.WriteLine("");
+            Console.WriteLine("poque você me esquece e some!");
+            Console.WriteLine("E se eu me interessar por alguém!");
+            Console.WriteLine("e se de repente ela me ganha!");
+            Console.WriteLine("");
+            Console.WriteLine("quando a gente gosta!");
+            Console.WriteLine("É claro que a gente cuida!");
+            Console.WriteLine("fala que me ama!");
+            Console.WriteLine("só que é da boca pra fora!");
+            Console.WriteLine("");
+            Console.WriteLine("ou você me engana!");
+            Console.WriteLine("ou não esta madura!");
+            Console.WriteLine("onde está você agora!");
+            
+        
+        }
+    }
+}
